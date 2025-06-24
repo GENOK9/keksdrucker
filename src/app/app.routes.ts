c452aa7bfd34f724ae28607f1cf77f25643a2c99
Dominik <dominik@prengel.info>
2025-06-24 20:48:13 +0200

    Routing zwischen Settings und Game Hinzugefügt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {GameComponent} from './game/game.component';
+import {EinstellungenComponent} from './einstellungen/einstellungen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"",
+    redirectTo:"game",
+    pathMatch:"full"
+  },
+  {path:"game",
+  component:GameComponent
+  },
+  {
+    path:"settings",
+    component:EinstellungenComponent
+  }
+
+];
+
