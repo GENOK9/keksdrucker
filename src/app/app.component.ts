5c4b9a1d3bdf8b4cfae197cacaeed7a845a99d7d
Dominik <dominik@prengel.info>
2025-06-24 22:34:18 +0200

    Routing zwischen Settings und Game Hinzugefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,15 @@ import { GameComponent } from "./game/game.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GameComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'keksdrucker';
+
+  ngOnInit() {
+    const isDark = localStorage.getItem('darkMode') === 'true';
+    document.body.classList.toggle('dark-mode', isDark);
+  }
 }
