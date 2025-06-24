3200a10eff892524e7324fb66bd22c5fb0d4185e
Dominik <dominik@prengel.info>
2025-06-24 20:48:13 +0200

    Routing zwischen Settings und Game Hinzugefügt

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card"
+import {RouterLink} from '@angular/router';
+
 @Component({
   selector: 'app-game',
-  imports: [MatIconModule, MatButtonModule, MatCardModule],
+  imports: [MatIconModule, MatButtonModule, MatCardModule, RouterLink],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
