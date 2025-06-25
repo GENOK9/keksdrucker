4697d68985dfa63576a6b7fef7a9dda5f8b92b95
Dominik <dominik@prengel.info>
2025-06-25 17:18:54 +0200

    Css ist hübsche

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,19 +4,20 @@ import { MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card"
 import {RouterLink} from '@angular/router';
 import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import {UpgradesComponent} from '../upgrades/upgrades.component';
 
 @Component({
   selector: 'app-game',
-  imports: [MatIconModule, MatButtonModule, MatCardModule, RouterLink, MatGridList, MatGridTile],
+  imports: [MatIconModule, MatButtonModule, MatCardModule, RouterLink, MatGridList, MatGridTile, UpgradesComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
 export class GameComponent {
-  cookies:number = localStorage.getItem('zaehler') === null ? 0 : Number(localStorage.getItem('zaehler'));
+  cookies:number = localStorage.getItem('cookies') === null ? 0 : Number(localStorage.getItem('cookies'));
 
   keksPress(){
     this.cookies++;
-    localStorage.setItem('zaehler', String(this.cookies));
+    localStorage.setItem('cookies', String(this.cookies));
   }
   setSettingButtonMode():string{
     const darkMode = localStorage.getItem('darkMode');
