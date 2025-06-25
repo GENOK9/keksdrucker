037010cd6063f11559d9ede2c98ad38d9cfad1da
Dominik <dominik@prengel.info>
2025-06-25 15:17:55 +0200

    Css ist hübsche

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,20 +3,29 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card"
 import {RouterLink} from '@angular/router';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-game',
-  imports: [MatIconModule, MatButtonModule, MatCardModule, RouterLink],
+  imports: [MatIconModule, MatButtonModule, MatCardModule, RouterLink, MatGridList, MatGridTile],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
 export class GameComponent {
-  zaehler:number = localStorage.getItem('zaehler') === null ? 0 : Number(localStorage.getItem('zaehler'));
+  cookies:number = localStorage.getItem('zaehler') === null ? 0 : Number(localStorage.getItem('zaehler'));
 
   keksPress(){
-    this.zaehler++;
-    localStorage.setItem('zaehler', String(this.zaehler));
+    this.cookies++;
+    localStorage.setItem('zaehler', String(this.cookies));
+  }
+  setSettingButtonMode():string{
+    const darkMode = localStorage.getItem('darkMode');
+    return darkMode === 'true' ? 'settingsButtonDark' : 'settingsButtonLight';
   }
 
+  setTitleTheme():string{
+    const darkMode = localStorage.getItem('darkMode');
+    return darkMode === 'true' ? 'titleDarkMode' : 'titleLightMode';
+  }
 
 }
